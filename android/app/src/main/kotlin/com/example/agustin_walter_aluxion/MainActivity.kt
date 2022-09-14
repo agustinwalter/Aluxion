26ece3603331011d2af34bf7c39a4fcf70c0f356
Agustín Walter <agustin.walter98@gmail.com>
2022-09-14 10:08:28 +0200

    Initial commit.

---
+++ android/app/src/main/kotlin/com/example/agustin_walter_aluxion/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.agustin_walter_aluxion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const Aluxion());
+
+class Aluxion extends StatelessWidget {
+  const Aluxion({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aluxion',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Museo Sans',
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
